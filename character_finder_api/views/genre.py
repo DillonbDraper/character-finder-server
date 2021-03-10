3cfed41ab5994f997092a54128f1df75e2f03247
Dillon Draper <dillonbdraper@gmail.com>
2021-03-10 09:18:58 -0600

    Overrode create method to make sure it only worked for admins

--- character_finder_api/views/genre.py
+++ character_finder_api/views/genre.py
@@ -1,6 +1,5 @@
 from django.core.exceptions import ValidationError
 from rest_framework import status
-from django.http import HttpResponseServerError
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 from rest_framework.response import Response
@@ -20,4 +19,23 @@ class Genres(mixins.CreateModelMixin,
                                 GenericViewSet):
     serializer_class = GenreSerializer
     queryset = Genre.objects.all()
+
+    def create(self, request):
+        genre = Genre()
+        genre.name = request.data['name']
+        user = request.auth.user
+
+        if user.is_staff is True:
+            try:
+                genre.save()
+                serializer = GenreSerializer(
+                    genre, context={'request': request})
+                return Response(serializer.data)
+            except ValidationError as ex:
+                return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+        else:
+            return Response({"reason": "This is an admin only action"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
 
