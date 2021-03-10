a1c169ac2612ac8c186c6996616fd50e1f727377
Dillon Draper <dillonbdraper@gmail.com>
2021-03-09 22:36:45 -0600

    updated viewset for genres to be a custom viewset modeled after django rest framework documentation for allowing create/read/retrieve but not update/destroy.  Need to learn how to efficiently set permissions for each method and development will be greatly expedited, but this is good for now.

--- character_finder_api/views/genre.py
+++ character_finder_api/views/genre.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
 from rest_framework import status
 from django.http import HttpResponseServerError
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
 from rest_framework.response import Response
 from rest_framework import serializers
 from character_finder_api.models import Genre
@@ -12,7 +13,11 @@ class GenreSerializer(serializers.ModelSerializer):
         model = Genre
         fields = ('id', 'name', )
 
-class Genres(ModelViewSet):
+
+class Genres(mixins.CreateModelMixin,
+                                mixins.ListModelMixin,
+                                mixins.RetrieveModelMixin,
+                                GenericViewSet):
     serializer_class = GenreSerializer
     queryset = Genre.objects.all()
 
