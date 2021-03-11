fbba948a0e3882d6bd66ae72012d2fc842a5ff4c
Dillon Draper <dillonbdraper@gmail.com>
2021-03-11 15:50:02 -0600

    succeeded in getting associated characters to appear on the author on retrieve method, but it is clear now project will need a serializers directory to remain as dry as possible and avoid circular import issues.

--- character_finder_api/views/author.py
+++ character_finder_api/views/author.py
@@ -1,15 +1,25 @@
+from character_finder_api.models.characters import Character
 from character_finder_api.models import Author, Reader
 from rest_framework import serializers, status
 from rest_framework.viewsets import ModelViewSet
 from django.core.exceptions import ValidationError
 from rest_framework.response import Response
+from django.http import HttpResponseServerError
 from character_finder_api.permissions import UserPermission
 
-class AuthorSerializer(serializers.ModelSerializer):
+class GenericCharacterSerializer(serializers.ModelSerializer):
 
     class Meta:
+        model = Character
+        depth = 1
+        fields = ('id', 'reader', 'name', 'age', 'born_on', 'died_on', 'alias', 'bio', 'public_version',)
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    characters = GenericCharacterSerializer(many=True)
+    class Meta:
         model = Author
-        fields = ('id', 'reader', 'name', 'born_on', 'died_on', 'bio',)
+        fields = ('id', 'reader', 'name', 'born_on', 'died_on', 'bio', 'characters')
 
 class Authors(ModelViewSet):
     queryset = Author.objects.all()
@@ -32,3 +42,25 @@ class Authors(ModelViewSet):
             return Response(serializer.data)
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single character
+        Returns:
+            Response -- JSON serialized character instance
+        """
+        try:
+            # `pk` is a parameter to this function, and
+            # Django parses it from the URL route parameter
+            #   http://localhost:8000/categories/2
+            #
+            # The `2` at the end of the route becomes `pk`
+            author = Author.objects.get(pk=pk)
+
+            author.characters = Character.objects.filter(fiction_char__fiction__author_fiction__author=author)
+            
+            serializer = AuthorSerializer(author, context={'request': request})
+
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+        
