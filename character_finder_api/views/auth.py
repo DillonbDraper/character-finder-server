260c40c7d6e4cbd508afa6fa240343f3e3210ee8
Dillon Draper <dillonbdraper@gmail.com>
2021-03-13 17:21:49 -0600

    Fixed settings that were causing issues with CORS policy headers (seemed to be in middleware and not whitelist, but not sure) and customized login/register functions to work for this app.'

--- character_finder_api/views/auth.py
+++ character_finder_api/views/auth.py
@@ -2,6 +2,7 @@ import json
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from character_finder_api.models import Reader
 from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
@@ -37,14 +38,23 @@ def register_user(request):
     # Create a new user by invoking the `create_user` helper method
     # on Django's built-in User model
     new_user = User.objects.create_user(
+        first_name=req_body['firstName'],
+        last_name=req_body['lastName'],
         username=req_body['username'],
         email=req_body['email'],
-        password=req_body['password']
+        password=req_body['password'],
+        is_staff=1
+    )
+
+    reader = Reader.objects.create(
+        user=new_user,
+        name=req_body['firstName'] + ' ' + req_body['lastName']
     )
     # Commit the user to the database by saving it
     new_user.save()
     # Use the REST Framework's token generator on the new user account
     token = Token.objects.create(user=new_user)
+    reader.save()
     # Return the token to the client
     data = json.dumps({"token": token.key})
     return HttpResponse(data, content_type='application/json')
